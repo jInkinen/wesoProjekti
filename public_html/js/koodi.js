2f7e4bae6b4e443ed6573fb2f95b3918d88db4cf
jInkinen <juha.inkinen@hotmail.com>
2012-12-18 08:39:25 +0200

    uutissyöte jsonista

--- public_html/js/koodi.js
+++ public_html/js/koodi.js
@@ -1,34 +1,20 @@
-"use strict";
-
-var site = {
-    view: {},
-    model: {}
-};
-
-site.view.Etusivu = Backbone.View.extend({
-    el: $("#etusivu"),
-    initialize: function() {
-        this.model.bind("change", this.render, this);
-        this.render();
-    },
-    render: function() {
-        var html = Mustache.render($("#etusivu-keski-temp").html(), this.model.attributes);
-        this.$el.html(html);
-    }
-});
-
-site.model.Uutiset = Backbone.Model.extend({
-    initialize: function() {
-        this.set("data", seconds);
-    }
-});
+var data = {};
 
 $(document).ready(function() {
-    
-    
-    
-    var uutiset = new site.model.Uutiset(data);
-    new site.view.ClockView({
-        model: clock
-    });
+    haeJSON();
 });
+
+var haeJSON = function() {
+    $.getJSON("data/newsfeed.json",
+            "",
+            function(dta) {
+                data = dta;
+                console.log(data);
+                render();
+            });
+};
+
+var render = function() {
+    var html = Mustache.render($("#etusivu-keski-temp").html(), data);
+    this.$("#keski").html(html);
+};
