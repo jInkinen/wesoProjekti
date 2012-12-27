c4053f13f660a789d0ff4ed34753d326ae025d13
jInkinen <juha.inkinen@hotmail.com>
2012-12-27 11:58:35 +0200

    rss-syötteet etusivulle

--- public_html/js/koodi.js
+++ public_html/js/koodi.js
@@ -3,25 +3,50 @@ var etu = $("#etusivu");
 var tut = $("#tutkimus");
 var opi = $("#opiskelu");
 
-var data = {};
+var etusivuPaauutiset = {};
+var etusivuAjankohtaistaFeed = {items: [], luokka: "#ajankohtaista"};
+var etusivuPaatapahtumatFeed = {items: [], luokka: "#paatapahtumat"};
 
 $(document).ready(function() {
     tut.hide();
     opi.hide();
+
     haeJSON();
+    haeRSS("http://www.cs.helsinki.fi/news/92/feed", etusivuAjankohtaistaFeed, 5);
+    haeRSS("http://www.cs.helsinki.fi/news/179/feed", etusivuPaatapahtumatFeed, 2);
 });
 
 var haeJSON = function() {
     $.getJSON("data/newsfeed.json",
             "",
             function(dta) {
-                data = dta;
-                console.log(data);
-                render();
+                etusivuPaauutiset = dta;
+                //console.log(data);
+                render("#keski", "#etusivu-keski-temp", etusivuPaauutiset);
             });
 };
 
-var render = function() {
-    var html = Mustache.render($("#etusivu-keski-temp").html(), data);
-    this.$("#keski").html(html);
+var haeRSS = function(osoite, olio, maara) {
+    $.get(osoite, function(data) {
+        var $xml = $(data);
+
+        var rss = $xml.find("item");
+        
+        for (var ind = 0; ind < maara; ind++) {
+            console.log(rss);
+            rssData = {
+                title: rss.find("title")[ind].textContent,
+                link: rss.find("link")[ind].textContent
+            };
+            
+            olio["items"].push(rssData);
+        }
+        //console.log(olio);
+        render(olio["luokka"], "#etusivu-rss-temp", olio);
+    });
+};
+
+var render = function(kohde, temp, data) {
+    var html = Mustache.render($(temp).html(), data);
+    $(kohde).html(html);
 };
