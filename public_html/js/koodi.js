953bfb6c401ce3449c226995057ac3cf97ecf1fa
jInkinen <juha.inkinen@hotmail.com>
2013-01-08 14:31:08 +0200

    palautuskuosi

--- public_html/js/koodi.js
+++ public_html/js/koodi.js
@@ -1,3 +1,9 @@
+// Sisältää sivuston toiminnallisuuden kannalta oleelliset javascript-tiedot
+// 
+// jQuery on käytössä tiettyjä asioita helpottamaan.
+// Hyödyntää Backbone-kirjastoa (joka tarvitsee toimiakseen underscore.js-
+// kirjaston). Lisäksi käytössä on Mustache.js sivujen renderöintiä varten.
+
 "use strict";
 
 var site = {
@@ -5,23 +11,31 @@ var site = {
     model: {}
 };
 
+// VIEW-määrittelyt
+
 site.view.Etusivu = Backbone.View.extend({
+    // liitetään view linkkiin, josta se avataan
     el: $("#0"),
+    // luodaan viewille kokoelma, johon näytettävät tiedot tallennetaan
     initialize: function() {
         this.collection = new site.model.Uutiset();
         this.collection.bind("reset", this.render, this);
         this.collection.bind("change", this.render, this);
         this.collection.fetch();
     },
+    // Mustache.js:a hyödyntävä renderöinti
     render: function() {
         var html = Mustache.render($("#etusivu-keski-temp").html(), this.collection.models[0].attributes);
         $("#keski").html(html);
     },
+    // Piilotetaan muut kuin haluttu sivuston osa ja vaihdetaan sivun title
     show: function() {
         $("#etusivu").show();
         $("#tutkimus").hide();
         $("#opiskelu").hide();
+        document.title = "Etusivu | Tietojenkäsittelytieteen laitos";
     },
+    // sidotaan linkin klikkaus näyttämään haluttu osio sivustosta
     events: {
         "click": function() {
             this.show();
@@ -30,22 +44,28 @@ site.view.Etusivu = Backbone.View.extend({
 });
 
 site.view.Tutkimus = Backbone.View.extend({
+    // liitetään view linkkiin, josta se avataan
     el: $("#1"),
+    // luodaan viewille kokoelma, johon näytettävät tiedot tallennetaan
     initialize: function() {
         this.collection = new site.model.Tutkijat();
         this.collection.bind("reset", this.render, this);
         this.collection.bind("change", this.render, this);
         this.collection.fetch();
     },
+    // Mustache.js:a hyödyntävä renderöinti
     render: function() {
         var html = Mustache.render($("#tutkimus-sisus-temp").html(), this.collection.models[0].attributes);
         $("#tutkimus-sisus").html(html);
     },
+    // Piilotetaan muut kuin haluttu sivuston osa ja vaihdetaan sivun title
     show: function() {
         $("#etusivu").hide();
         $("#tutkimus").show();
         $("#opiskelu").hide();
+        document.title = "Tutkimus | Tietojenkäsittelytieteen laitos";
     },
+    // sidotaan linkin klikkaus näyttämään haluttu osio sivustosta
     events: {
         "click": function() {
             this.show();
@@ -54,12 +74,16 @@ site.view.Tutkimus = Backbone.View.extend({
 });
 
 site.view.Opiskelu = Backbone.View.extend({
+    // liitetään view linkkiin, josta se avataan
     el: $("#2"),
+    // Piilotetaan muut kuin haluttu sivuston osa ja vaihdetaan sivun title
     show: function() {
         $("#etusivu").hide();
         $("#tutkimus").hide();
         $("#opiskelu").show();
+        document.title = "Opiskelu | Tietojenkäsittelytieteen laitos";
     },
+    // sidotaan linkin klikkaus näyttämään haluttu osio sivustosta
     events: {
         "click": function() {
             this.show();
@@ -68,11 +92,13 @@ site.view.Opiskelu = Backbone.View.extend({
 });
 
 site.view.RSSajankohtaista = Backbone.View.extend({
+    // luodaan viewille malli, johon näytettävät syötteet tallennetaan
     initialize: function() {
         this.model = new site.model.RSS();
         this.model.bind("change", this.render, this);
         this.model.lataaSyote("http://www.cs.helsinki.fi/news/92/feed", 3);
     },
+    // Mustache.js:a hyödyntävä renderöinti
     render: function() {
         var html = Mustache.render($("#etusivu-rss-temp").html(), this.model.attributes);
         $("#ajankohtaista").html(html);
@@ -80,11 +106,13 @@ site.view.RSSajankohtaista = Backbone.View.extend({
 });
 
 site.view.RSSpaatapahtumat = Backbone.View.extend({
+    // luodaan viewille malli, johon näytettävät syötteet tallennetaan
     initialize: function() {
         this.model = new site.model.RSS();
         this.model.bind("change", this.render, this);
         this.model.lataaSyote("http://www.cs.helsinki.fi/tapahtumat/179/feed", 2);
     },
+    // Mustache.js:a hyödyntävä renderöinti
     render: function() {
         var html = Mustache.render($("#etusivu-rss-temp").html(), this.model.attributes);
         $("#paatapahtumat").html(html);
@@ -92,59 +120,75 @@ site.view.RSSpaatapahtumat = Backbone.View.extend({
 });
 
 site.view.RSSopiskelu = Backbone.View.extend({
+    // luodaan viewille malli, johon näytettävät syötteet tallennetaan
     initialize: function() {
         this.model = new site.model.RSS();
         this.model.bind("change", this.render, this);
         this.model.lataaSyote("http://www.cs.helsinki.fi/news/94/feed", 4);
     },
+    // Mustache.js:a hyödyntävä renderöinti
     render: function() {
         var html = Mustache.render($("#etusivu-rss-temp").html(), this.model.attributes);
         $("#opuutiset").html(html);
     }
 });
 
+
+
+// MODEL-määrittelyt
+
+// RSS-feedien lukemiseen käytettävä model
 site.model.RSS = Backbone.Model.extend({
+    //asettaa oletusarvon muuttujalle items
     initialize: function() {
         this.set({"items": []});
     },
+    //lataa syötteen halutusta osoitteesta halutulla määrällä uutisia
     lataaSyote: function(osoite, maara) {
         var rssData = {};
         var isanta = this;
+        
         $.get(osoite, function(data) {
             var $xml = $(data);
             var rss = $xml.find("item");
             for (var ind = 0; ind < maara; ind++) {
+                //tallennettava data muotoillaan sopivaksi olioksi
                 rssData = {
                     title: rss.find("title")[ind].textContent,
                     link: rss.find("link")[ind].textContent
                 };
                 //this tässä kontekstissa viittaa jQuery.get-kyselyyn
+                //joten käytetään isanta apumuuttujaa haetun datan tallentamiseen
                 isanta.tallenna(rssData);
             }
         });
     },
     tallenna: function(uusiData) {
+        //Lisätään modeliin uusi data, joka tulee parametrinä funktiolle
         var data = this.get("items");
         data.push(uusiData);
         this.set({"items": data});
-        //Laukaistaan change, jotta view päivittää renderöidyn sivun
+        //Laukaistaan change, jotta view päivittää uuden datan sivulle
         this.trigger('change',this);
     }
 });
 
+//uutiset- ja tutkijat-kokoelmien käyttämä apuluokka
 site.model.Artikkeli = Backbone.Model.extend();
 
+//uutiset-kokoelman määritelmät
 site.model.Uutiset = Backbone.Collection.extend({
     model: site.model.Artikkeli,
     url: "data/newsfeed.json"
 });
 
+//tutkijat-kokoelman määritelmät
 site.model.Tutkijat = Backbone.Collection.extend({
     model: site.model.Artikkeli,
     url: "data/tutkimus.json"
 });
 
-//Dokumentin alustus
+// Dokumentin alustus
 $(document).ready(function() {
     //Etusivun osat
     var etu = new site.view.Etusivu();
@@ -158,33 +202,3 @@ $(document).ready(function() {
     //Asetetaan etusivu näkymään oletuksena, kun sivu avataan
     etu.show();
 });
-
-
-/*
- 
- 
- haeJSON("data/newsfeed.json", etusivuPaauutiset, "#keski", "#etusivu-keski-temp");
- haeJSON("data/tutkimus.json", tutkimusRyhmat, "#tutkimus-sisus", "#tutkimus-sisus-temp");
- 
- haeRSS("http://www.cs.helsinki.fi/news/92/feed", etusivuAjankohtaistaFeed, 5);
- haeRSS("http://www.cs.helsinki.fi/tapahtumat/179/feed", etusivuPaatapahtumatFeed, 2);
- haeRSS("http://www.cs.helsinki.fi/news/94/feed", opiskeluUutisetFeed, 5);
- 
- var render = function(kohde, temp, data) {
- var html = Mustache.render($(temp).html(), data);
- $(kohde).html(html);
- };
- 
- var show = function(numero) {
- etu.hide();
- tut.hide();
- opi.hide();
- 
- sivut[numero].show();
- };
- 
- var addListeners = function() {
- $("#navi a").click(function() {
- show(this.id);
- });
- };*/
